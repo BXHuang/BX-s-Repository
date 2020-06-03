74a781f7727290dd8a6c019bc7bd5d29538d6496
BXHuang <66353352+BXHuang@users.noreply.github.com>
2020-06-03 16:51:33 +0800

    Update download_pix2pix_dataset.sh

--- datasets/download_pix2pix_dataset.sh
+++ datasets/download_pix2pix_dataset.sh
@@ -1,6 +1,6 @@
 FILE=$1
 
-if [[ $FILE != "cityscapes" && $FILE != "night2day" && $FILE != "edges2handbags" && $FILE != "edges2shoes" && $FILE != "facades" && $FILE != "maps" ]]; then
+if [[ $FILE != "cityscapes" && $FILE != "night2day" && $FILE != "edges2handbags" && $FILE != "edges2shoes" && $FILE != "pixelat" && $FILE != "facades" && $FILE != "maps" ]]; then
   echo "Available datasets are cityscapes, night2day, edges2handbags, edges2shoes, facades, maps"
   exit 1
 fi
@@ -13,10 +13,10 @@ fi
 
 echo "Specified [$FILE]"
 
-URL=http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/$FILE.tar.gz
-TAR_FILE=./datasets/$FILE.tar.gz
-TARGET_DIR=./datasets/$FILE/
+URL=https://drive.google.com/drive/folders/1igmhP9o8rAB2FpYp02d9y4pAG0ofE5Je?usp=sharing/$FILE.tar.gz
+TAR_FILE=./1igmhP9o8rAB2FpYp02d9y4pAG0ofE5Je?usp=sharing/$FILE.tar.gz
+TARGET_DIR=./1igmhP9o8rAB2FpYp02d9y4pAG0ofE5Je?usp=sharing/$FILE/
 wget -N $URL -O $TAR_FILE
 mkdir -p $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./datasets/
+tar -zxvf $TAR_FILE -C ./1igmhP9o8rAB2FpYp02d9y4pAG0ofE5Je?usp=sharing/
 rm $TAR_FILE
